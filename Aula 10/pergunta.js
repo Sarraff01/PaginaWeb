c98855c7814426ebc2c0a7a354efa3cc7224d676
Vinicius Sarraff <152625137+Sarraff01@users.noreply.github.com>
2024-10-17 12:04:12 -0300

    Add files via upload

---
+++ Aula 10/pergunta.js
@@ -0,0 +1,22 @@
+criaCard(
+    'Programação',
+    'O que é JavaScript?',
+    'JavaScript é uma linguagem de programação que permite a você implementar itens complexos em páginas web'
+)
+
+criaCard(
+    'Programação',
+    'O que é CSS',
+    'CSS é uma linguagem de estilização'
+)
+
+criaCard(
+    'Geografia',
+    'O que foi a Guerra Fria?',
+    'Um conflito indireto entre EUA e URSS'
+)
+criaCard(
+    'Carros',
+    'De onde é a Volkswagen?',
+    'Alemanha'
+)
